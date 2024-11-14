16256053bf3b880969a83b25f549f0543f6abeb5
Meemsoossaas <rayyankom@gmail.com>
2024-11-14 07:39:26 +0100

    Initial commit

---
+++ example/meem_base_build_example.dart
@@ -0,0 +1,6 @@
+import 'package:meem_base_build/meem_base_build.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/meem_base_build_test.dart
@@ -0,0 +1,16 @@
+import 'package:meem_base_build/meem_base_build.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
